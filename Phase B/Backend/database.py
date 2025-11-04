5ac283c1969d0c569e27c0a8dd0677fd10c7d786
David <zorin98@gmail.com>
2025-11-04 21:15:47 +0200

    Getting Started: Setup FastAPI backend with MongoDB and Beanie

---
+++ Phase B/Backend/database.py
@@ -0,0 +1,14 @@
+import os
+from beanie import init_beanie
+from dotenv import load_dotenv
+import motor.motor_asyncio
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    DATABASE_URL, uuidRepresentation="standard"
+)
+db = client["main"]
