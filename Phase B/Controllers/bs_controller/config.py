a21ac64362b3add2585a817e80daa0c5250a019a
tomerot <tomer.rotman97@gmail.com>
2025-12-31 20:37:12 +0200

    Barcode Scanner Controller: Basics Done

---
+++ Phase B/Controllers/bs_controller/config.py
@@ -0,0 +1,8 @@
+import os
+
+# --- Log File Path ---
+LOG_FILE_PATH = "./logs"
+
+# --- Local WebSocket Server Configuration ---
+LOCAL_WS_HOST = "localhost"
+LOCAL_WS_PORT = 8766
---
+++ Phase B/Controllers/bs_controller/scanner/scanner_handler.py
@@ -0,0 +1,2 @@
+class ScannerHandler:
+    pass
